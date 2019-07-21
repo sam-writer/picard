8b4f9419385a3fb8c4b42632ec144c326b6d5c04
Sam Havens <sam.havens@qordoba.com>
2019-07-20 19:41:07 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ export const App = () => {
 
       <h2>Picard Watchlist</h2>
 
-      <p>I am stoked about the upcoming series <a href="https://arstechnica.com/gaming/2019/07/once-again-engage-picard-trailer-feels-like-the-next-next-generation/">Picard</a>.
-      I found some watchlists, but needed a spreadsheet to make sure I watched in order.</p>
-      <p>This list is open-source, so if you have edits, open a Pull Request <a href="ADD LINK">here</a>.</p>
+      <p>I am <i>quite</i> stoked about the upcoming NEW STAR TREK SERIES, <a href="https://arstechnica.com/gaming/2019/07/once-again-engage-picard-trailer-feels-like-the-next-next-generation/">Picard</a>.
+      I found some watch lists, but once you have more than one watch list, it's hard to understand the order. Also, spreasheets are amazing.
+      So I sucked it up, acknowledged that I am now the biggest nerd ever, and made this.</p>
+      <p>This list is open-source, so if you have suggestions, open a Pull Request <a href="https://github.com/sam-qordoba/picard">here</a>.</p>
+      <br /><br />
+      <p><a href="https://twitter.com/sam_havens?ref_src=twsrc%5Etfw" class="twitter-follow-button"
+        data-show-count="false">Follow @sam_havens</a>
+      </p>
       <br /><br />
       <CsvToHtmlTable
         data={picardCsv}
